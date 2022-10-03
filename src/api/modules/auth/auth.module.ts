01bf89e8f0e92c556f98f7ff517ddab7f65634f7
asimpat <asimoko@yahoo.com>
2022-10-03 11:31:00 +0100

    added user and auth

---
+++ src/api/modules/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entity/entities';
+import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constant';
+import { PassportModule } from '@nestjs/passport';
+import { jwtStrategy } from './stragtegy/jwtstrategy';
+
+
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UserService, jwtStrategy],
+
+})
+export class AuthModule {}
---
+++ src/api/modules/auth/stragtegy/jwtstrategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from '../constant';
+
+
+@Injectable()
+export class jwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    const { password, ...result } = payload;
+    return result;
+  }
+}
