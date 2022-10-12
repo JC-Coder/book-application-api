326533840e084547ee8efa676ab763d757b0903e
asimpat <asimoko@yahoo.com>
2022-10-12 12:40:48 +0100

    modified swagger doc for user

--- src/api/modules/user/user.controller.ts
+++ src/api/modules/user/user.controller.ts
@@ -17,77 +17,79 @@ import { UserService } from './user.service';
 import { Response } from 'express';
 import { JwtAuthGuard } from '../auth/Guards/authGuard';
 import { UserGuard } from '../auth/Guards/userGuard';
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiProperty, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
 
-  @ApiTags('User')
-  @Controller('user')
-  export class UserController {
-    constructor(
-      private readonly userService: UserService,
-      private readonly authService: AuthService,
-    ) {}
+@ApiTags('User')
+@Controller('user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
-    // register request
-    @Post('register')
-    @ApiCreatedResponse({
-      description: 'Created User ',
-      type: User,
-    })
-    @ApiBadRequestResponse({ description: 'Registration Failed, Try Again!!!' })
-    async register(@Body() user: CreateUserDto): Promise<User> {
-      return await this.userService.registerUser(user);
-    }
-
-    // login request
-    @Post('login')
-    @ApiCreatedResponse({
-      description: 'Login Sucessful',
-    })
-    @ApiBadRequestResponse({
-      description: 'Credentials Incorrect, Try Again!!!',
-    })
-    async login(
-      @Body() user: LoginDto,
-      @Res({ passthrough: true }) response: Response,
-    ) {
-      return await this.authService.login(user, response);
-    }
-
-    // logout request
-    @Post('logout')
-    @ApiCreatedResponse({
-      description: 'LogOut Sucessful',
-    })
-    async logout(
-      @Res({ passthrough: true }) response: Response,
-    ): Promise<User | Object> {
-      return await this.authService.logout(response);
-    }
-
-    // Authorization of users
-    @UseGuards(JwtAuthGuard)
-    @ApiCreatedResponse({
-      description: 'User Authorized',
-    })
-    @ApiBadRequestResponse({
-      description: 'User UnAuthorized, Confirm token is invalid',
-    })
-    @Get('profile')
-    async getUser(@Request() req) {
-      return await req.user;
-    }
-
-    //User update
-    @UseGuards(JwtAuthGuard, UserGuard)
-    @ApiCreatedResponse({
-      description: 'User is Updated',
-    })
-    @ApiBadRequestResponse({
-      description: 'Only User can update Profile, login as a user',
-    })
-    @Put(':id')
-    async updateUser(@Param('id') id, @Body() user) {
-      return await this.userService.updateUser(id, user);
-    }
-   
+  // register request
+  @Post('register')
+  @ApiCreatedResponse({ description: 'Registered Succesfully', type: User })
+  @ApiUnprocessableEntityResponse({ description: 'Bad Request. Try again!!' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  async register(@Body() user: CreateUserDto): Promise<User|Object> {
+    return await this.userService.registerUser(user);
   }
+
+  // login request
+  @Post('login')
+  @ApiOkResponse({
+    description: 'The resource was returned successfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  async login(
+    @Body() user: LoginDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return await this.authService.login(user, response);
+  }
+
+  // logout request
+  @Post('logout')
+  @ApiOkResponse({
+    description: 'The resource was returned successfully',
+  })
+  @ApiUnprocessableEntityResponse({ description: 'Bad Request' })
+  async logout(
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<User | Object> {
+    return await this.authService.logout(response);
+  }
+
+  // Authorization of users
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ description: 'The resource was returned successfully' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @Get('profile')
+  async getUser(@Request() req) {
+    return await req.user;
+  }
+
+  //User update Profile
+  @UseGuards(JwtAuthGuard, UserGuard)
+  @ApiOkResponse({ description: 'The resource was updated successfully' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiUnprocessableEntityResponse({ description: 'Bad Request' })
+  @Put(':id')
+  async updateUser(@Param('id') id, @Body() user) {
+    return await this.userService.updateUser(id, user);
+  }
+}
