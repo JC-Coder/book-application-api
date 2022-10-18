0fc1b9792a9881cf6a983d5d30d464da58d03621
JC-Coder <99423608+JC-Coder@users.noreply.github.com>
2022-10-18 18:46:49 +0100

    updated

--- src/api/config/typeorm.config.ts
+++ src/api/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL || ' postgres://postgres:root@localhost:5432/book_app_api';,
+  url: process.env.DATABASE_URL || ' postgres://postgres:root@localhost:5432/book_app_api',
   autoLoadEntities: true,
   synchronize: true,
   dropSchema: true,
