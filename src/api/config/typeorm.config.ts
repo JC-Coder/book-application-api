7a0b01c2a2adaa10e9dba6e1cd6cbb92fdab5e3e
JC-Coder <josephchimezie2003@gmail.com>
2022-10-18 17:49:13 +0100

    changed db credentials to postgres

--- src/api/config/typeorm.config.ts
+++ src/api/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.MYSQLHOST || 'localhost',
-  username: process.env.MYSQLUSER || 'root',
-  // port: +process.env.MYSQLPORT,
-  password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || 'book_app_api',
+  type: 'postgres',
+  host: process.env.PGHOST || 'localhost',
+  username: process.env.PGUSER || 'postgres',
+  port: +process.env.PGPORT || 5432,
+  password: process.env.PGPASSWORD || 'root',
+  database: process.env.PGDATABASE || 'book_app_api',
   autoLoadEntities: true,
   synchronize: true,
   dropSchema: false,
