d21e523491e1d06d1c2d51441868a0c13df610bb
JC-Coder <99423608+JC-Coder@users.noreply.github.com>
2022-10-18 17:52:41 +0100

    updated file

--- src/api/config/typeorm.config.ts
+++ src/api/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.MYSQLHOST || 'adaptable-prod.database.windows.net',
-  username: process.env.MYSQLUSER || 'bookstore-main-db-060f6f16c7f4a981f',
-  port: +process.env.MYSQLPORT || 1433,
-  password: process.env.MYSQLPASSWORD || 'EavUB3UUQUEB8AY2SdKNY5Jts4RbK2',
-  database: process.env.MYSQLDATABASE || 'bookstore-main-db-060f6f16c7f4a981f',
+  type: 'postgres',
+  host: process.env.PGHOST || 'localhost',
+  username: process.env.PGUSER || 'postgres',
+  port: +process.env.PGPORT || 5432,
+  password: process.env.PGPASSWORD || 'root',
+  database: process.env.PGDATABASE || 'book_app_api',
   autoLoadEntities: true,
   synchronize: true,
   dropSchema: false,
